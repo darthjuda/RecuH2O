8f4e696fff2b05b569e522b8d2d433311815a092
kiruahunter <mullerloic0901@gmail.com>
2020-06-03 17:45:37 +0200

    ajout programme
    j'ai commencé a définir les entrées et les sorties et j'ai réfléchi aux fonctions a utiliser

--- RecuH2O_arduino/RecuH2O_arduino.ino
+++ RecuH2O_arduino/RecuH2O_arduino.ino
@@ -1,9 +1,27 @@
+int graphbar = 5;
+int motor = 6;
+int valeur = 0;
+float niveaudeau;
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(graphbar, OUTPUT);
+pinMode(motor, OUTPUT);
+pinMode(valeur, INPUT);
+//définir les différents niveaux 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+switch (expression)
+{
+case /* constant-expression */:
+  /* code */
+  break;
 
+default:
+  break;
+  //permet de choisir entre plusieurs alternative
+
+  // une fonction qui permet de prendre la tension la plus haute ci cela existe
+}
 }
