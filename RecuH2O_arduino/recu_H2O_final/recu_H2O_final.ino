6d70c5ef9467fb7bd12e83ecad1b3d0b9bec7fc6
kiruahunter <mullerloic0901@gmail.com>
2020-08-15 15:35:26 +0200

    ajout programme
    ajouter la fonctionnalité wifi, la fonctionnalité serveur web et le design du serveur web

---
+++ RecuH2O_arduino/recu_H2O_final/recu_H2O_final.ino
@@ -0,0 +1,290 @@
+#include <Servo.h>
+#include <ESP8266WiFi.h>
+
+#ifndef STASSID
+#define STASSID "Reseau Wi-Fi de mehdi"
+#define STAPSK  "emyTourniquet1988"
+#endif
+
+// pointeur vers les infos ssid et password
+const char *ssid = STASSID;
+const char *password = STAPSK;
+
+//création de l'objet server de type WiFiServer en écoute sur le port 80
+WiFiServer server(80);
+Servo motor;
+
+//varable pour stocker la requete HTTP
+String header;
+String adressIP;
+
+// variables auxiliaires pour stocker l'état des sorties
+String motorState = "off";
+int niveauCuveState = niveauCuve
+String sondeState = "off";
+
+// assigner des variables en sorties aux pins GPIO
+const int motor.attach = 46;
+const int ledPins = {
+  14, 15, 16, 17, 18, 19, 20, 21
+};
+
+const int sonde = 0;
+
+//current time
+unsigned long currentTime = millis();
+
+//previous time
+unsigned long previousTime = 0;
+
+// on definit timeoutTime en millisecondes
+const long timeoutTime = 2000;
+////////////////////////////// graphBar //////////////////////////////////////////////////////////
+
+//permet de dire que le graphbar possede 10 led
+const int ledCount = 8;
+
+// les leds sont definies sur les pins ci dessous:
+int ledPins[] = {
+  14, 15, 16, 17, 18, 19, 20, 21
+};
+
+////////////////////////////// niveau /////////////////////////////////////////////////////////////
+
+int niveauCuve = 8;
+bool isOpen;
+const int maxLvl = ledCount;
+
+////////////////////////////// mesure capacité ////////////////////////////////////////////////////
+
+#define analogPin      0          // on utilise le pin A0 pour mesurer la tension du condensateur
+#define chargePin      13         // on utilise le pin 13 pour charger le condensateur - connected to one end of the charging resistor
+#define dischargePin   11         // on utilise le pin 11 pour décharger le condensateur
+#define resistorValue  10000.0F   // on entre la valeur de la resistance que l'on utilise
+// le F permet de mettre la valeur de la resistance en float
+unsigned long startTime;
+unsigned long elapsedTime;
+float microFarads;                // on définit la variable microFarads en float pour garder la precision et faire des calculs
+float nanoFarads;
+float mesureSonde;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  //initialiser les variables en sorties
+  pinMode(46, OUTPUT);
+  pinMode(ledPins, OUTPUT);
+  pinMode(sonde, OUTPUT);
+
+  // on met les sorties en LOW
+  digitalWrite(46, LOW);
+  digitalWrite(ledPins, LOW);
+  digitalWrite(sonde, LOW);
+
+  // connexion au réseau Wi-Fi avec le SSID et le mot de passe
+  Serial.print("Tentative de connexion: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED){
+    deay(500);
+    Serial.print(".");
+  }
+
+  //afficher les adresses IP locales et démarrer le serveur web
+  Serial.println("");
+  Serial.println("WiFi connecté.");
+  Serial.println("Adresse IP: ");
+  adressIP = WiFi.localIP();
+  Serial.println(adressIP);
+  server.begin();
+
+////////////////////////////// graphBar //////////////////////////////////////////////////////////
+
+// initialisation graphBar sortie:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT);
+  }
+
+////////////////////////////// mesure capacité ///////////////////////////////////////////////////
+
+  pinMode(chargePin, OUTPUT);     // on met le pin de chargement de la sonde en sortie
+  digitalWrite(chargePin, LOW);
+
+}
+
+////////////////////////////// loop //////////////////////////////////////////////////////////////
+
+void loop() {
+ 
+  WiFiClient client = server.available(); //écouter l'arrivé d'un cient
+
+  if (client){  // si un nouveau client se connecte
+    Serial.println("Nouveau client. "); // on écrit un message sur le port série
+    String currentLine = ""; // faire une chaine de caractere pour contenir les données du client entrant
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // faire une boucle tant que le client est connecté
+      currentTime = millis();
+      if(client.available()){ // s'il y a des bits a lire provenant du client
+        char c = client.read(); // on lit un paquet
+        Serial.write(c); //on l'ecrit sur le moniteur serie
+        header += c; //on ajoute les bits que l'on vient de lire a la variable (String) 'header'
+        if (c== '\n'){ //  si le bit est une nouvelle ligne de caractere
+          // si la ligne courante est vide, on a 2 caractere de retour a la ligne un a cote de l'autre
+          // c'est la fin de la requete HTTP du client, on envoi la reponse:
+          if (CurrentLine.length() == 0 {
+            // les en-tetes HTTP commencent toujours avec un code reponse (ex: HTTP/1.1 200 OK)
+            // et un type de contenue comme ca le client sait quelle type de donnée arrive
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("connection: close");
+            client.println();
+
+            // faire qqch si le client envoie qqch de speciale 
+
+
+            // Affichage page web en HTML et CSS 
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // code CSS pour le design de la page web
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #0394fc; border: 2px solid black; border-radius: 5px; color: black; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}");
+            client.println(".buttonCuveVert {background-color: #12c412; border: 2px solid black; border-radius: 5px; color: black;");
+            client.println(".buttonCuveOrange {background-color: #ff7b00; border: 2px solid black; border-radius: 5px; color: black;");
+            client.println(".buttonCuveRouge {background-color: #ff0000; border: 2px solid black; border-radius: 5px; color: black;</style></head>");
+
+            // En tete page web 
+            client.println("<body><h1> Recu H20 </h1>");
+
+            // Affichage des variables d'etats 
+            /*
+             * // variables auxiliaires pour stocker l'état des sorties
+                  String motorState = "off";
+                  String ledPinsState = "off";
+                  String sondeState = "off";
+            */
+            // Etat moteur
+            client.println("<p>Etat du clapet: " + motorState + "</p>");
+            // si etat clapet(moteur) = off affiché on sur le bouton
+            if (motorState=="off") {
+              client.println("<p><a href=\"addresse CONTROL PIN MOTOR"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"addresse CONTROL PIN MOTOR"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            // Etat led 
+            client.println("<p>Etat du niveau de la cuve: " + niveauCuveState + "</p>");
+            // TODO: if niveau cuve 1<lvl<4 button vert ...
+              
+            // Etat sonde
+
+            
+            
+          }
+        }
+      }
+    }
+    }
+
+  switch (niveauCuve) {
+    case 0 ...9:
+      openGate();
+      Serial.println(isOpen);
+      break;
+
+    case maxLvl:
+      closeGate();
+      Serial.println(isOpen);
+      break;
+  }
+
+  graphBarDisplay(niveauCuve);
+
+  mesureCapSonde();
+  Serial.println(mesureSonde);
+}
+
+void openGate() {
+  if (isOpen) {
+    Serial.println("clapet deja ouvert");
+  }
+  else {
+    for (int pos = 90; pos >= 0; pos--) { // on passe de la position 90 a 0 en décrémentant de 1 pas a chaque tour de la boucle
+      motor.write(pos); // on dit au moteur de se mettre a la valeur de pos
+      delay(15);// on attend 15 ms pour laisser le temps au moteur de se mettre en position
+    }
+    isOpen = true;
+    Serial.println("ouverture clapet");
+  }
+}
+
+void closeGate() {
+  if (!isOpen) {
+    Serial.println("clapet deja ferme");
+  }
+  else {
+    for (int pos = 0; pos <= 90; pos++) { // on passe de la position 0 a 90 en incrémentant de 1 pas a chaque tour de la boucle
+      motor.write(pos); // on dit au moteur de se mettre a la valeur de pos
+      delay(15); // on attend 15 ms pour laisser le temps au moteur de se mettre en position
+    }
+    isOpen = false;
+    Serial.println("fermeture clapet");
+  }
+}
+
+void graphBarDisplay(int niveau) {
+  niveau -= 1; // on enlève 1 a la valeur du niveau
+  for (int i = ledCount; i >= niveau; i--) { // pour i allant au nombre de led (10) a la valeur du niveau, on décrémente i de 1
+    digitalWrite(ledPins[i], LOW); // on éteind la led correspondant a la valeur de i
+  }
+
+  for (int i = 0; i <= niveau; i++) { // pour i allant de 0 a la valeur du niveau, on incrémente i de 1
+    digitalWrite(ledPins[i], HIGH); // on allume la led correspondant a la valeur de i
+  }
+}
+
+float mesureCapSonde() {
+  digitalWrite(chargePin, HIGH);  // on charge le condensateur 
+  startTime = millis();
+
+  while (analogRead(analogPin) < 648) {  // 647 correspond a  63.2% de 1023, qui correspond a la tension maximum
+  }
+
+  elapsedTime = millis() - startTime;
+  // on convertit les millisecondes en secondes ( 10^-3 )
+  microFarads = ((float)elapsedTime / resistorValue) * 1000;
+  //  Serial.print(elapsedTime);       // on ecrit la valeur dans le moniteur serie
+  //  Serial.print(" mS    ");         // on ecrit l'unite dans le moniteur serie
+
+
+  if (microFarads > 1) {
+    mesureSonde = microFarads;
+    //    Serial.print((long)microFarads);       // on ecrit la valeur dans le moniteur serie
+    //    Serial.println(" microFarads");        // on ecrit l'unite dans le moniteur serie
+  }
+  else
+  {
+    // si la valeur est plus petite que le microFarads, on la convertit en nanoFarads (10^-9 Farad).
+    // c'est une solution de rechange car le Serial.print ne peux pas ecrire de float
+
+    nanoFarads = microFarads * 1000.0; // on multiplie par 1000 pour convertir en nanoFarads (10^-9 Farads)
+    mesureSonde = nanoFarads;
+    //    Serial.print((long)nanoFarads);         // on ecrit la valeur dans le moniteur serie
+    //    Serial.println(" nanoFarads");          // on ecrit l'unite dans le moniteur serie
+  }
+
+  /* dicharge the capacitor  */
+  digitalWrite(chargePin, LOW);             // on stoppe l'alimentation du condensateur
+  pinMode(dischargePin, OUTPUT);            // on met le pin de dechargement en sortie
+  digitalWrite(dischargePin, LOW);          // on alimente le pin de dechargement 
+  while (analogRead(analogPin) > 0) {       // tant que la valeur lue sur le pin A0 est superieur a 0
+  }
+
+  pinMode(dischargePin, INPUT);            // on met la broche de dechargement en entree
+
+  return mesureSonde;
+}
---
+++ protoArduino/protoRecuH2O/_autosave-protoRecuH2O.sch
@@ -0,0 +1,292 @@
+EESchema Schematic File Version 4
+EELAYER 30 0
+EELAYER END
+$Descr A4 11693 8268
+encoding utf-8
+Sheet 1 1
+Title ""
+Date ""
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndDescr
+$Comp
+L LED:HDSP-4830_2 BAR?
+U 1 1 5EE0DBEF
+P 3750 3550
+F 0 "BAR?" H 3750 2750 50  0000 C CNN
+F 1 "HDSP-4830_2" H 3800 2850 50  0000 C CNN
+F 2 "Display:HDSP-4830" H 3750 2750 50  0001 C CNN
+F 3 "https://docs.broadcom.com/docs/AV02-1798EN" H 1750 3750 50  0001 C CNN
+	1    3750 3550
+	1    0    0    -1  
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE1329C
+P 4250 4050
+F 0 "R?" V 4300 4250 50  0000 C CNN
+F 1 "330" V 4300 3850 50  0000 C CNN
+F 2 "" V 4180 4050 50  0001 C CNN
+F 3 "~" H 4250 4050 50  0001 C CNN
+	1    4250 4050
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE1456C
+P 4250 3950
+F 0 "R?" V 4300 4150 50  0000 C CNN
+F 1 "330" V 4300 3750 50  0000 C CNN
+F 2 "" V 4180 3950 50  0001 C CNN
+F 3 "~" H 4250 3950 50  0001 C CNN
+	1    4250 3950
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE147D1
+P 4250 3850
+F 0 "R?" V 4300 4050 50  0000 C CNN
+F 1 "330" V 4300 3650 50  0000 C CNN
+F 2 "" V 4180 3850 50  0001 C CNN
+F 3 "~" H 4250 3850 50  0001 C CNN
+	1    4250 3850
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE148DB
+P 4250 3750
+F 0 "R?" V 4300 3950 50  0000 C CNN
+F 1 "330" V 4300 3550 50  0000 C CNN
+F 2 "" V 4180 3750 50  0001 C CNN
+F 3 "~" H 4250 3750 50  0001 C CNN
+	1    4250 3750
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE14A29
+P 4250 3650
+F 0 "R?" V 4300 3850 50  0000 C CNN
+F 1 "330" V 4300 3450 50  0000 C CNN
+F 2 "" V 4180 3650 50  0001 C CNN
+F 3 "~" H 4250 3650 50  0001 C CNN
+	1    4250 3650
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE14C29
+P 4250 3550
+F 0 "R?" V 4300 3750 50  0000 C CNN
+F 1 "330" V 4300 3350 50  0000 C CNN
+F 2 "" V 4180 3550 50  0001 C CNN
+F 3 "~" H 4250 3550 50  0001 C CNN
+	1    4250 3550
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE14D78
+P 4250 3450
+F 0 "R?" V 4300 3650 50  0000 C CNN
+F 1 "330" V 4300 3250 50  0000 C CNN
+F 2 "" V 4180 3450 50  0001 C CNN
+F 3 "~" H 4250 3450 50  0001 C CNN
+	1    4250 3450
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE14F5B
+P 4250 3350
+F 0 "R?" V 4300 3550 50  0000 C CNN
+F 1 "330" V 4300 3150 50  0000 C CNN
+F 2 "" V 4180 3350 50  0001 C CNN
+F 3 "~" H 4250 3350 50  0001 C CNN
+	1    4250 3350
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE150FA
+P 4250 3250
+F 0 "R?" V 4300 3450 50  0000 C CNN
+F 1 "330" V 4300 3050 50  0000 C CNN
+F 2 "" V 4180 3250 50  0001 C CNN
+F 3 "~" H 4250 3250 50  0001 C CNN
+	1    4250 3250
+	0    1    1    0   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5EE15322
+P 4250 3150
+F 0 "R?" V 4300 3350 50  0000 C CNN
+F 1 "330" V 4300 2950 50  0000 C CNN
+F 2 "" V 4180 3150 50  0001 C CNN
+F 3 "~" H 4250 3150 50  0001 C CNN
+	1    4250 3150
+	0    1    1    0   
+$EndComp
+Wire Wire Line
+	3950 3150 4100 3150
+Wire Wire Line
+	3950 3250 4100 3250
+Wire Wire Line
+	3950 3350 4100 3350
+Wire Wire Line
+	3950 3450 4100 3450
+Wire Wire Line
+	3950 3550 4100 3550
+Wire Wire Line
+	3950 3650 4100 3650
+Wire Wire Line
+	3950 3750 4100 3750
+Wire Wire Line
+	3950 3850 4100 3850
+Wire Wire Line
+	3950 3950 4100 3950
+Wire Wire Line
+	3950 4050 4100 4050
+Wire Wire Line
+	4750 3150 4750 3250
+Wire Wire Line
+	4750 3600 4950 3600
+Connection ~ 4750 3600
+Wire Wire Line
+	4750 3600 4750 3650
+$Comp
+L power:GNDREF #PWR?
+U 1 1 5EE19A2D
+P 4950 3600
+F 0 "#PWR?" H 4950 3350 50  0001 C CNN
+F 1 "GNDREF" V 4955 3472 50  0000 R CNN
+F 2 "" H 4950 3600 50  0001 C CNN
+F 3 "" H 4950 3600 50  0001 C CNN
+	1    4950 3600
+	0    -1   -1   0   
+$EndComp
+Connection ~ 4750 3250
+Wire Wire Line
+	4750 3250 4750 3350
+Connection ~ 4750 3350
+Wire Wire Line
+	4750 3350 4750 3450
+Connection ~ 4750 3450
+Wire Wire Line
+	4750 3450 4750 3550
+Connection ~ 4750 3550
+Wire Wire Line
+	4750 3550 4750 3600
+Connection ~ 4750 3650
+Wire Wire Line
+	4750 3650 4750 3750
+Connection ~ 4750 3750
+Wire Wire Line
+	4750 3750 4750 3850
+Connection ~ 4750 3850
+Wire Wire Line
+	4750 3850 4750 3950
+Connection ~ 4750 3950
+Wire Wire Line
+	4750 3950 4750 4050
+$Comp
+L Motor:Motor_Servo M?
+U 1 1 5EE1E1FF
+P 4350 2700
+F 0 "M?" H 4682 2765 50  0000 L CNN
+F 1 "Motor_Servo" H 4682 2674 50  0000 L CNN
+F 2 "" H 4350 2510 50  0001 C CNN
+F 3 "http://forums.parallax.com/uploads/attachments/46831/74481.png" H 4350 2510 50  0001 C CNN
+	1    4350 2700
+	1    0    0    -1  
+$EndComp
+Wire Wire Line
+	2950 4450 2950 4700
+Text Label 2950 4700 0    50   ~ 0
+GND
+Wire Wire Line
+	4050 2800 3900 2800
+Text Label 3900 2800 0    50   ~ 0
+GND
+Wire Wire Line
+	4050 2700 3900 2700
+Text Label 3900 2700 0    50   ~ 0
+VCC
+Wire Wire Line
+	2850 2350 2850 2200
+Text Label 2850 2200 1    50   ~ 0
+VCC
+$Comp
+L Amplifier_Operational:TL082 U?
+U 3 1 5EE26502
+P 1500 3150
+F 0 "U?" H 1150 3550 50  0000 L CNN
+F 1 "TL082" H 1100 3650 50  0000 L CNN
+F 2 "" H 1500 3150 50  0001 C CNN
+F 3 "http://www.ti.com/lit/ds/symlink/tl081.pdf" H 1500 3150 50  0001 C CNN
+	3    1500 3150
+	1    0    0    -1  
+$EndComp
+Wire Wire Line
+	3150 4450 3150 4700
+Text Label 3150 4700 0    50   ~ 0
+GND2
+Wire Wire Line
+	1400 3450 1400 3600
+Text Label 1400 3600 0    50   ~ 0
+GND2
+Wire Wire Line
+	3550 2950 3700 2950
+Wire Wire Line
+	1400 2850 1400 2800
+Text Label 3700 2950 0    50   ~ 0
+alimsonde
+Text Label 1400 2700 0    50   ~ 0
+alimsonde
+Wire Wire Line
+	4050 2600 3550 2600
+Text Label 3550 2600 0    50   ~ 0
+motor
+Text Label 2100 3550 0    50   ~ 0
+motor
+Wire Wire Line
+	2550 3550 2100 3550
+Wire Wire Line
+	4400 3150 4750 3150
+Wire Wire Line
+	4400 3250 4750 3250
+Wire Wire Line
+	4400 3350 4750 3350
+Wire Wire Line
+	4400 3450 4750 3450
+Wire Wire Line
+	4400 3550 4750 3550
+Wire Wire Line
+	4400 3650 4750 3650
+Wire Wire Line
+	4400 3750 4750 3750
+Wire Wire Line
+	4400 3850 4750 3850
+Wire Wire Line
+	4400 3950 4750 3950
+Wire Wire Line
+	4400 4050 4750 4050
+Wire Wire Line
+	1400 2800 1600 2800
+Connection ~ 1400 2800
+Wire Wire Line
+	1400 2800 1400 2700
+Wire Wire Line
+	2550 3350 2350 3350
+Text Label 1600 2800 0    50   ~ 0
+sonde
+Text Label 2350 3350 0    50   ~ 0
+sonde
+$EndSCHEMATC
