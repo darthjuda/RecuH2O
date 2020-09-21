7da3609d307056c48b9450d2fb332bf96e0a5b4d
darthjuda <mehdi.jung@me.com>
2020-09-21 14:33:26 +0200

    R
    Signed-off-by: darthjuda <mehdi.jung@me.com>

--- RecuH2O_arduino/recu_H2O_final/recu_H2O_final.ino
+++ RecuH2O_arduino/recu_H2O_final/recu_H2O_final.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
 #include <ESP8266WiFi.h>
+#include "dessinCuve.h"
 
 #ifndef STASSID
 #define STASSID "Reseau Wi-Fi de mehdi"
@@ -10,13 +11,11 @@
 const char *ssid = STASSID;
 const char *password = STAPSK;
 
-//création de l'objet server de type WiFiServer en écoute sur le port 80
-WiFiServer server(80);
-Servo motor;
 
 //varable pour stocker la requete HTTP
 String header;
 String adressIP;
+
 
 ////////////////////////////// graphBar //////////////////////////////////////////////////////////
 
@@ -24,8 +23,8 @@ String adressIP;
 const int ledCount = 8;
 
 // les leds sont definies sur les pins ci dessous:
-const int ledPins[] = {
-  14, 15, 16, 17, 18, 19, 20, 21
+const uint8_t ledPins[] = {
+  D0, D1, D2, D3, D5, D6, D7, D8
 };
 
 
@@ -54,9 +53,9 @@ const long timeoutTime = 2000;
 
 ////////////////////////////// mesure capacité ////////////////////////////////////////////////////
 
-#define analogPin      0          // on utilise le pin A0 pour mesurer la tension du condensateur
-#define chargePin      13         // on utilise le pin 13 pour charger le condensateur - connected to one end of the charging resistor
-#define dischargePin   11         // on utilise le pin 11 pour décharger le condensateur
+#define analogPin      A0          // on utilise le pin A0 pour mesurer la tension du condensateur
+#define chargePin      D4         // on utilise le pin 13 pour charger le condensateur - connected to one end of the charging resistor
+#define dischargePin   D9         // on utilise le pin 11 pour décharger le condensateur
 #define resistorValue  10000.0F   // on entre la valeur de la resistance que l'on utilise
 // le F permet de mettre la valeur de la resistance en float
 unsigned long startTime;
@@ -64,6 +63,13 @@ unsigned long elapsedTime;
 float microFarads;                // on définit la variable microFarads en float pour garder la precision et faire des calculs
 float nanoFarads;
 float mesureSonde;
+
+
+//création de l'objet server de type WiFiServer en écoute sur le port 80
+WiFiServer server(80);
+Servo motor;
+
+String dessinCuve = MAIN_dessinCuve;
 
 void setup() {
   // put your setup code here, to run once:
@@ -74,24 +80,24 @@ void setup() {
 
   //initialiser les variables en sorties
   pinMode(46, OUTPUT);
-  for(int i=0;i<=ledCount;i++) {
+  for (int i = 0; i <= ledCount; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
   pinMode(sonde, OUTPUT);
 
   // on met les sorties en LOW
   digitalWrite(46, LOW);
-  for(int i=0;i<=ledCount;i++) {
-    digitalWrite(ledPins[i], LOW);  
+  for (int i = 0; i <= ledCount; i++) {
+    digitalWrite(ledPins[i], LOW);
   }
-  
+
   digitalWrite(sonde, LOW);
 
   // connexion au réseau Wi-Fi avec le SSID et le mot de passe
   Serial.print("Tentative de connexion: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -103,14 +109,14 @@ void setup() {
   Serial.println(WiFi.localIP());
   server.begin();
 
-////////////////////////////// graphBar //////////////////////////////////////////////////////////
+  ////////////////////////////// graphBar //////////////////////////////////////////////////////////
 
-// initialisation graphBar sortie:
+  // initialisation graphBar sortie:
   for (int thisLed = 0; thisLed < ledCount; thisLed++) {
     pinMode(ledPins[thisLed], OUTPUT);
   }
 
-////////////////////////////// mesure capacité ///////////////////////////////////////////////////
+  ////////////////////////////// mesure capacité ///////////////////////////////////////////////////
 
   pinMode(chargePin, OUTPUT);     // on met le pin de chargement de la sonde en sortie
   digitalWrite(chargePin, LOW);
@@ -120,21 +126,21 @@ void setup() {
 ////////////////////////////// loop //////////////////////////////////////////////////////////////
 
 void loop() {
- 
+
   WiFiClient client = server.available(); //écouter l'arrivé d'un cient
 
-  if (client){  // si un nouveau client se connecte
+  if (client) { // si un nouveau client se connecte
     Serial.println("Nouveau client. "); // on écrit un message sur le port série
     String currentLine = ""; // faire une chaine de caractere pour contenir les données du client entrant
     currentTime = millis();
     previousTime = currentTime;
     while (client.connected() && currentTime - previousTime <= timeoutTime) { // faire une boucle tant que le client est connecté
       currentTime = millis();
-      if(client.available()){ // s'il y a des bits a lire provenant du client
+      if (client.available()) { // s'il y a des bits a lire provenant du client
         char c = client.read(); // on lit un paquet
         Serial.write(c); //on l'ecrit sur le moniteur serie
         header += c; //on ajoute les bits que l'on vient de lire a la variable (String) 'header'
-        if (c== '\n'){ //  si le bit est une nouvelle ligne de caractere
+        if (c == '\n') { //  si le bit est une nouvelle ligne de caractere
           // si la ligne courante est vide, on a 2 caractere de retour a la ligne un a cote de l'autre
           // c'est la fin de la requete HTTP du client, on envoi la reponse:
           if (currentLine.length() == 0) {
@@ -145,10 +151,10 @@ void loop() {
             client.println("connection: close");
             client.println();
 
-            // faire qqch si le client envoie qqch de speciale 
+            // faire qqch si le client envoie qqch de speciale
 
 
-            // Affichage page web en HTML et CSS 
+            // Affichage page web en HTML et CSS
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
@@ -157,19 +163,19 @@ void loop() {
             client.println(".button { background-color: #0394fc; border: 2px solid black; border-radius: 5px; color: black; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 { background-color: #77878A;}");
-            client.println(".buttonCuveVert { background-color: #12c412; border: 2px solid black; border-radius: 5px; color: black;}");
-            client.println(".buttonCuveOrange { background-color: #ff7b00; border: 2px solid black; border-radius: 5px; color: black;}");
-            client.println(".buttonCuveRouge { background-color: #ff0000; border: 2px solid black; border-radius: 5px; color: black;}</style></head>");
+            client.println(".dot { background-color: #12c412; border: 2px solid black; border-radius: 5px; color: black;}");
+            client.println(".dot { background-color: #ff7b00; border: 2px solid black; border-radius: 5px; color: black;}");
+            client.println(".dot { background-color: #ff0000; border: 2px solid black; border-radius: 5px; color: black;}</style></head>");
 
-            // En tete page web 
+            // En tete page web
             client.println("<body><h1> Recu H20 </h1>");
 
-            // Affichage des variables d'etats 
-            
+            // Affichage des variables d'etats
+
             // Etat moteur
             client.println("<p>Etat du clapet: " + motorState + "</p>");
             // si etat clapet(moteur) = off affiché on sur le bouton
-            if (motorState=="off") {
+            if (motorState == "off") {
               //client.println("<p><a href=\"/addresseCONTROLPINMOTOR"><button class=\"button\">ON</button></a></p>");
               client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
             } else {
@@ -177,39 +183,40 @@ void loop() {
               client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
-            // Etat led 
+            // Etat led
             client.println("<p>Etat du niveau de la cuve: " + niveauCuveState + "</p>");
+            client.print(dessinCuve);
 
             switch (niveauCuve) {
               case 0 ...3:
-                client.println("<p><button class=\"button buttonCuveVert\"></button></p>");
-              break;
+                client.println("<p><span class=\"dot\">Bas</span></p>");
+                break;
 
               case 4 ...6:
                 client.println("<p><button class=\"button buttonCuveOrange\"></button></p>");
-              break;
+                break;
 
               case 7 ...8:
                 client.println("<p><button class=\"button buttonCuveRouge\"></button></p>");
-              break;
+                break;
 
-              default: 
+              default:
                 client.println("<p>Probleme avec la variable du niveau de la cuve</p>");
             }
-              
+
             // Etat sonde
-              // TODO: afficher l'etat de la sonde en web
-            
+            // TODO: afficher l'etat de la sonde en web
+
             client.println("</body></html>");
-            // fin de la reponse http 
+            // fin de la reponse http
             client.println();
             break;
-            
+
           } else { // Si reception d'une nouvelle ligne reset currentLine
             currentLine = "";
           }
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
-            currentLine += c;      // add it to the end of the currentLine
+          currentLine += c;      // add it to the end of the currentLine
         }
       }
     }
@@ -220,22 +227,22 @@ void loop() {
     Serial.println("Client déconnecté.");
     Serial.println("");
 
-//  switch (niveauCuve) {
-//    case 0 ...7:
-//      openGate();s
-//      Serial.println(isOpen);
-//      break;
-//
-//    case maxLvl:
-//      closeGate();
-//      Serial.println(isOpen);
-//      break;
-//  }
-//
-//  graphBarDisplay(niveauCuve);
-//
-//  mesureCapSonde();
-//  Serial.println(mesureSonde);
+    switch (niveauCuve) {
+      case 0 ...7:
+        openGate(); 
+        Serial.println(isOpen);
+        break;
+
+      case maxLvl:
+        closeGate();
+        Serial.println(isOpen);
+        break;
+    }
+
+    graphBarDisplay(niveauCuve);
+
+//    mesureCapSonde();
+//    Serial.println(mesureSonde);
   }
 }
 
@@ -279,7 +286,7 @@ void graphBarDisplay(int niveau) {
 }
 
 float mesureCapSonde() {
-  digitalWrite(chargePin, HIGH);  // on charge le condensateur 
+  digitalWrite(chargePin, HIGH);  // on charge le condensateur
   startTime = millis();
 
   while (analogRead(analogPin) < 648) {  // 647 correspond a  63.2% de 1023, qui correspond a la tension maximum
@@ -311,7 +318,7 @@ float mesureCapSonde() {
   /* dicharge the capacitor  */
   digitalWrite(chargePin, LOW);             // on stoppe l'alimentation du condensateur
   pinMode(dischargePin, OUTPUT);            // on met le pin de dechargement en sortie
-  digitalWrite(dischargePin, LOW);          // on alimente le pin de dechargement 
+  digitalWrite(dischargePin, LOW);          // on alimente le pin de dechargement
   while (analogRead(analogPin) > 0) {       // tant que la valeur lue sur le pin A0 est superieur a 0
   }
 
