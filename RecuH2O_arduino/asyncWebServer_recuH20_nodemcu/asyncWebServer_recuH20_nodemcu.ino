011d6a91675323cc52dfdbe09fedd9591ba8e3a6
kiruahunter <mullerloic0901@gmail.com>
2020-11-23 15:05:00 +0100

    Update asyncWebServer_recuH20_nodemcu.ino
    correction du code de la sonde

--- RecuH2O_arduino/asyncWebServer_recuH20_nodemcu/asyncWebServer_recuH20_nodemcu.ino
+++ RecuH2O_arduino/asyncWebServer_recuH20_nodemcu/asyncWebServer_recuH20_nodemcu.ino
@@ -49,8 +49,11 @@ String sondeState;
 // entier pour sauvegarder le niveau
 int niveau;
 
+// valeur de la capacitance lorsque la cuve est vide Ɛ
+const float capEmpty;
+
 // valeur de la capacitance lorsque la cuve est pleine
-float capFull;
+const float capFull;
 
 //valeur de la capacitance à l'instant ou l'on mesure
 float capInstant;
@@ -75,7 +78,45 @@ String getPressure() {
 
 int measureSonde() {
 
-  analogRead(A0); // mesure de la capacitance //FAUX!!!!!!!!
+   digitalWrite(chargePin, HIGH);  // on charge le condensateur 
+  startTime = millis(); // on démarre le timer
+
+   while (analogRead(analogPin) < 648) {  // 647 correspond a  63.2% de 1023, qui correspond a la tension maximum
+  } // tant que le nombre de bits lu sur A0 n'est pas égal à 647, on ne fait rien
+
+   elapsedTime = millis() - startTime;
+
+   microFarads = ((float)elapsedTime / resistorValue) * 1000; // on convertit les millisecondes en secondes (10^-3)
+
+  Serial.print(elapsedTime);       // on ecrit la valeur dans le moniteur serie
+  Serial.print(" mS    ");         // on ecrit l'unite dans le moniteur serie 
+
+  if (microFarads > 1) {
+    mesureSonde = microFarads; 
+    Serial.print((long)microFarads);       // on ecrit la valeur dans le moniteur serie
+    Serial.println(" microFarads");  // on ecrit l'unite dans le moniteur serie
+  }
+
+  else
+  {
+    // si la valeur est plus petite que le microFarads, on la convertit en nanoFarads (10^-9 Farad).
+    // c'est une solution de rechange car le Serial.print ne peux pas ecrire de float
+
+    nanoFarads = microFarads * 1000.0; // on multiplie par 1000 pour convertir en nanoFarads (10^-9 Farads)
+    mesureSonde = nanoFarads;
+    Serial.print((long)nanoFarads);         // on ecrit la valeur dans le moniteur serie
+    Serial.println(" nanoFarads");          // on ecrit l'unite dans le moniteur serie
+  }
+
+  // décharger le condensateur
+  digitalWrite(chargePin, LOW);             // on stoppe l'alimentation du condensateur
+  pinMode(dischargePin, OUTPUT);            // on met le pin de dechargement en sortie
+  digitalWrite(dischargePin, LOW);          // on alimente le pin de dechargement 
+  while (analogRead(analogPin) > 0) {       // tant que la valeur lue sur le pin A0 est superieur a 0
+  }
+
+  pinMode(dischargePin, INPUT);            // on met la broche de dechargement en entree
+
   niveau = ((capInstant*100)/capFull) //transforme la capacitance en niveau en %
   niveau = (niveau*6)/100 // on transforme le niveau en % en niveau compris entre 0 et 6
   return int(niveau)
